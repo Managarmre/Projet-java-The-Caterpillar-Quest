20d3185370515f5672d1003fd5c53518d3a8de5e
syffer <maxime.nicolas2@hotmail.fr>
2015-05-08 19:11:18 +0200

    Test d'ouverture d'une fenêtre de jeu (OK)

---
+++ ProjetJava/src/FenetreTest.java
@@ -0,0 +1,42 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+public class FenetreTest extends BasicGame  {
+
+	public FenetreTest(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main( String[] args ) {
+		try {
+			new AppGameContainer(new FenetreTest("test"), 640, 480, false).start();
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
