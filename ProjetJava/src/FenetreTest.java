cfe73b635575b8c7551a8833bbb19bd9221835a1
syffer <maxime.nicolas2@hotmail.fr>
2015-05-12 23:31:32 +0200

    ajout d'image 32x32
    cerise, plateforme, porte

--- ProjetJava/src/FenetreTest.java
+++ ProjetJava/src/FenetreTest.java
@@ -1,38 +1,59 @@
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
 
 public class FenetreTest extends BasicGame  {
 
+	private Animation animation;
+	
+	private static int LARGEUR = 32 * 20;
+	private static int HAUTEUR = 32 * 10;
+	
 	public FenetreTest(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
+	public void init( GameContainer container ) throws SlickException {
 		// TODO Auto-generated method stub
 		
+		SpriteSheet spriteSheet = new SpriteSheet("sprites/plateforme.png", 32, 32);
+		
+		this.animation = new Animation();
+		this.animation.addFrame( spriteSheet.getSprite(0, 0), 100);
+		
 	}
-
+	
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
+	public void render( GameContainer container, Graphics g ) throws SlickException {
+		// TODO Auto-generated method stub
+		
+		g.setColor(new Color(50, 50, 50, 180));
+		g.fillRect(0, 0, 300, 70);
+		g.setColor(Color.white);
+		
+		g.drawAnimation( this.animation, 0, 0 );
+		g.drawAnimation( this.animation, 64, 32 );
+	}
+	
+	@Override
+	public void update( GameContainer container, int delta ) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
 	
 	public static void main( String[] args ) {
 		try {
-			new AppGameContainer(new FenetreTest("test"), 640, 480, false).start();
+			new AppGameContainer(new FenetreTest("test"), LARGEUR, HAUTEUR, false).start();
 		} catch (SlickException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
