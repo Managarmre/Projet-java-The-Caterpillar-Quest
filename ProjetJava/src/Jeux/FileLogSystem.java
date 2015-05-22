bf1ed7ccc2788d5936125a4234009e598c930164
syffer <maxime.nicolas2@hotmail.fr>
2015-05-22 19:15:32 +0200

    documentation

--- ProjetJava/src/Jeux/FileLogSystem.java
+++ ProjetJava/src/Jeux/FileLogSystem.java
@@ -4,22 +4,41 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 
-import org.newdawn.slick.util.DefaultLogSystem;
+import org.newdawn.slick.util.LogSystem;
 
-public class FileLogSystem extends DefaultLogSystem {
+/**
+ * Classe permettant de rediriger les logs de Slick2D vers un fichier de log. 
+ * Pour rediriger les logs, il faut utiliser Log.setLogSystem(...).
+ * Pour plus d'information, voir https://github.com/ariejan/slick2d/blob/master/src/org/newdawn/slick/util/DefaultLogSystem.java.
+ *  
+ * @author Maxime Pineau
+ * @see <a href="http://slick.ninjacave.com/javadoc/org/newdawn/slick/util/LogSystem.html">LogSystem</a>
+ * 
+ */
+public class FileLogSystem implements LogSystem {
 
-	// https://github.com/ariejan/slick2d/blob/master/src/org/newdawn/slick/util/DefaultLogSystem.java
+	private String cheminFichierLog;; 
 	
-	private String cheminFichierLog = "TheQuaterpillarQuest.log"; 
-	
+	/**
+	 * Crée une instance de FileLogSystem qui redirigera les logs de Slick2D vers le fichier 'cheminFichierLog'.
+	 * @param cheminFichierLog Le chemin du fichier vers lequel les logs seront redirigés.
+	 */
 	public FileLogSystem( String cheminFichierLog ) {
 		this.cheminFichierLog = cheminFichierLog;
 	}
 	
+	/**
+	 * Crée une instance de FileLogSystem qui redirigera les logs de Slick2D vers le fichier 'TheQuaterpillarQuest.log'.
+	 */
 	public FileLogSystem() {
 		this("TheQuaterpillarQuest.log");
 	}
 	
+	
+	/**
+	 * Permet d'écrire un message dans le fichier log.
+	 * @param message Le message à écrire dans le fichier log.
+	 */
 	private void ecrireMessage( String message ) {
 
 		try {
