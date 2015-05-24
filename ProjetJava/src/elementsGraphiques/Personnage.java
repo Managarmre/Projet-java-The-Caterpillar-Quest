fc2975cf4ca8faed144df9f75d4082ecddca390e
syffer <maxime.nicolas2@hotmail.fr>
2015-05-25 00:15:44 +0200

    back in time....

--- ProjetJava/src/elementsGraphiques/Personnage.java
+++ ProjetJava/src/elementsGraphiques/Personnage.java
@@ -21,11 +21,6 @@ import org.newdawn.slick.geom.Rectangle;
 
 /**
  * @author Cyril
- * Représente le personnage jouable à l'écran
-
- * Représente le personnage du joueur, celui-ci pourra être déplacé sur la carte par le joueur avec son clavier.
- * @author Cyril Caron
- * @autor Maxime Pineau
  *
  */
 public class Personnage extends ElementDeplacable {
@@ -43,10 +38,9 @@ public class Personnage extends ElementDeplacable {
 	private float ay = 0.0f; // valeur de l'accélération
 	private float dx = 0.0f; // valeur du déplacement du personnage en X
 	private float dy = 0.0f; // valeur du déplacement du personnage en Y
-	private double tempsSaut = 0.5;
+	private double tempsSaut = 0.7;
 	
 	/**
-	 * Crée un personnage.
 	 * @param x La position en x du personnage
 	 * @param y La position en y du personnage
 	 */
@@ -77,72 +71,52 @@ public class Personnage extends ElementDeplacable {
 	@Override
 	public void update( GameContainer conteneur, int delta, Carte carte ) throws SlickException, PartieException {
 		
+		Point oldPosition;
 		
-		vx = (float) (delta * 0.015 * this.speed);
-		vy = (float) (delta * 0.05 * this.speed );
-
-		ay = (float) (vy * (delta/1000.0) / tempsSaut);
-
+		this.vx = (float) ( delta * 0.015 * this.speed );
+		this.vy = (float) ( delta * 0.05 * this.speed );
 		
-		if(direction == Direction.DROITE){
+		// accérération, à ajouté à dy pour crééer la gravité, ou retirer à dy pour créer le saut
+		this.ay = (float) ( this.vy * (delta/1000.0) / this.tempsSaut );	
+		
+		
+
+		// si le personnage est en l'air (pour se déplacer dans les airs)
+		if( this.jumping ) this.dy -= this.ay; 	// le personnage est en l'air
+		else { //si le joueur est au sol
+
+			if( this.direction == Direction.HAUT && this.isMoving ) {
 			
-			if(isMoving){ // on autorise le personnage à se déplacer une seule fois
-				dx = vx; // déplacement à droite
-			}				
-		}
-		else if(direction == Direction.GAUCHE){
-			
-			if(isMoving){
-				dx = -vx;// déplacement à gauche
-			}				
-		}
-		else 
-			dx = 0;
-
-		if(jumping && ! isCollisionOnTop){ 
-			dy -= ay;
-		}
-		
-		if( this.isCollisionOnTop && isMoving) // si on se déplace sur une plateforme
-			this.setPositionX(this.getPositionX() + dx);
-		
-		// on teste la collision en x
-		if(this.estEnCollisionAvecPlateforme(carte) && ! isCollisionOnTop){ // on ignore les collisions avec le haut de la plateforme
-			dx = -dx;
-			this.setPositionX(this.getPositionX() + dx);
-			dx = 0;
-		} 
-
-		
-		if(! jumping){ //si le joueur est au sol
-
-			if(direction == Direction.HAUT){
+				this.dy = this.vy;
+				this.jumping = true; // le personnage va sauter
 				
-				if( isMoving){
-					dy = vy;
-					dx = vx;
-					this.jumping = true; // le personnage va sauter
-
-					this.setPosition(this.getPositionX() + dx, this.getPositionY() - dy);
-				}
-								
-			}else{
-				dy = 0; // on ne prend pas en compte le saut car le personnage est déjà en l'air
 			}
-			
-		}else{ // le personnage est en l'air
-			dy -= ay;	// on applique la gravité		
-			this.setPositionY( this.getPositionY() - dy);
+			// else : // on ne prend pas en compte le saut car le personnage est déjà en l'air
+		}
+		
+		oldPosition = this.getPosition();
+		this.setPositionY( this.getPositionY() - this.dy );
+		if( this.estEnCollisionAvecPlateforme(carte) && ! isCollisionOnTop ) {
+			this.setPositionY( oldPosition.getY() );
+			this.dy = 0;	// on remet l'accélération à 0
 		}
 		
 		
-		// on teste la collision en y
-		if(this.estEnCollisionAvecPlateforme(carte) && ! isCollisionOnTop){
-			dy = -dy; 
-			this.setPositionY(this.getPositionY() + dy);
-			dy = 0;
-		}
-
+		
+		
+		
+		// on autorise le personnage à se déplacer une seule fois
+		if( this.direction == Direction.DROITE && this.isMoving ) this.dx = this.vx; 	// déplacement à droite
+		else if( direction == Direction.GAUCHE && this.isMoving ) this.dx = - this.vx; 	// déplacement à gauche
+		else this.dx = 0;
+				
+		oldPosition = this.getPosition();		// on sauvegarde l'ancienne position
+		this.setPositionX( this.getPositionX() + this.dx );
+		if( this.estEnCollisionAvecPlateforme(carte) && ! this.isCollisionOnTop ) this.setPositionX( oldPosition.getX() );
+		
+		
+					
+		
 		ElementRamassable elementRamassable = carte.getElementRamassableEnCollisionAvecElement(this); 
 		if( elementRamassable != null ) {
 			this.nbPoints += elementRamassable.getNbPoints();
@@ -156,8 +130,6 @@ public class Personnage extends ElementDeplacable {
 		if( carte.elementEnCollisionAvecUnEnnemi(this) ) throw new PartiePerdueException();
 		
 		// le personnage sort de la fenêtre, le joueur perd la partie
-		
-		// le personnage sort de la fenêtre, le joueur perd la partie
 		if( this.getPositionY() > Jeu.HAUTEUR ) throw new PartiePerdueException();
 		
 	}
@@ -166,7 +138,7 @@ public class Personnage extends ElementDeplacable {
 	
 	/**
 	 * Indique si le personnage est en collision avec une plateforme
-	 * @param carte 
+	 * @param carte
 	 * @return
 	 */
 	public boolean estEnCollisionAvecPlateforme(Carte carte){
@@ -174,7 +146,8 @@ public class Personnage extends ElementDeplacable {
 		for( ElementFixe plateforme : carte.getElementsFixes() ) {	
 			if( this.estEnCollisionAvec(plateforme) ){
 				
-				if(this.getPositionY() + this.getHauteur() <= plateforme.getPositionY() + 15){ // collision en haut
+				// collision en haut
+				if( this.getPositionY() - plateforme.getPositionY() <= 0.1 ) {
 					this.setPositionY(plateforme.getPositionY() - this.getHauteur());
 					isCollisionOnTop = true;
 					jumping = false;
@@ -215,14 +188,9 @@ public class Personnage extends ElementDeplacable {
 	@Override
 	public void afficher(GameContainer conteneur, Graphics graphique) throws SlickException {
 		
-		super.afficher(conteneur, graphique);
 		graphique.drawAnimation( this.animations[0], this.getPositionX(), this.getPositionY() );
 	}
 
-	/**
-	 * Retourne le nombre de points qu'a obtenu le personnage.
-	 * @return Le nombre de points qu'a obtenu le personnage.
-	 */
 	public int getNbPoints() {
 		return this.nbPoints;
 	}
