e4735f3da0c3e7fb41a33650370f84c24df38936
syffer <maxime.nicolas2@hotmail.fr>
2015-05-24 23:28:01 +0200

    perso jump now !

--- ProjetJava/src/elementsGraphiques/Personnage.java
+++ ProjetJava/src/elementsGraphiques/Personnage.java
@@ -21,7 +21,7 @@ import org.newdawn.slick.geom.Rectangle;
 
 /**
  * @author Cyril
- * Représente le personnage jouable à l'écran
+ *
  */
 public class Personnage extends ElementDeplacable {
 	
@@ -38,7 +38,7 @@ public class Personnage extends ElementDeplacable {
 	private float ay = 0.0f; // valeur de l'accélération
 	private float dx = 0.0f; // valeur du déplacement du personnage en X
 	private float dy = 0.0f; // valeur du déplacement du personnage en Y
-	private double tempsSaut = 0.5;
+	private double tempsSaut = 0.7;
 	
 	/**
 	 * @param x La position en x du personnage
@@ -71,115 +71,58 @@ public class Personnage extends ElementDeplacable {
 	@Override
 	public void update( GameContainer conteneur, int delta, Carte carte ) throws SlickException, PartieException {
 		
+		Point oldPosition;
 		
-		vx = (float) (delta * 0.015 * this.speed);
-		vy = (float) (delta * 0.05 * this.speed );
-
-		ay = (float) (vy * (delta/1000.0) / tempsSaut);
-
+		this.vx = (float) ( delta * 0.015 * this.speed );
+		this.vy = (float) ( delta * 0.05 * this.speed );
 		
-		if(direction == Direction.DROITE){
+		// accérération, à ajouté à dy pour crééer la gravité, ou retirer à dy pour créer le saut
+		this.ay = (float) ( this.vy * (delta/1000.0) / this.tempsSaut );	
+		
+		
+
+		// si le personnage est en l'air (pour se déplacer dans les airs)
+		if( this.jumping ) this.dy -= this.ay; 	// le personnage est en l'air
+		else { //si le joueur est au sol
+
+			if( this.direction == Direction.HAUT && this.isMoving ) {
 			
-			if(isMoving){ // on autorise le personnage à se déplacer une seule fois
-				dx = vx; // déplacement à droite
-			}				
-		}
-		else if(direction == Direction.GAUCHE){
-			
-			if(isMoving){
-				dx = -vx;// déplacement à gauche
-			}				
-		}
-		else 
-			dx = 0;
-
-		if(isMoving){
-			
-			if(jumping && ! isCollisionOnTop){  // on autorise le déplacement pendant le saut
-				dy -= ay;
-			}
-
-		}
-		
-		if( this.isCollisionOnTop && isMoving) // si on se déplace sur une plateforme
-			this.setPositionX(this.getPositionX() + dx);
-		
-		// on teste la collision en x
-		if(this.estEnCollisionAvecPlateforme(carte) && ! isCollisionOnTop){ // on ignore les collisions avec le haut de la plateforme
-			dx = -dx;
-			this.setPositionX(this.getPositionX() + dx);
-			dx = 0;
-		} 
-
-		
-		if(! jumping){ //si le joueur est au sol
-
-			if(direction == Direction.HAUT){
+				this.dy = this.vy;
+				this.jumping = true; // le personnage va sauter
 				
-				if( isMoving){
-					dy = vy;
-					dx = vx;
-					this.jumping = true; // le personnage va sauter
-
-					this.setPosition(this.getPositionX() + dx, this.getPositionY() - dy);
-				}
-			}else{
-				dy = 0; // on ne prend pas en compte le saut car le personnage est déjà en l'air
 			}
-			
-		}else{ // le personnage est en l'air
-			dy -= ay;	// on applique la gravité		
-			this.setPositionY( this.getPositionY() - dy);
+			// else : // on ne prend pas en compte le saut car le personnage est déjà en l'air
 		}
 		
-		//this.setPositionY(this.getPositionY() - dy);
-		
-		// on teste la collision en y
-		if(this.estEnCollisionAvecPlateforme(carte) && ! isCollisionOnTop){
-			dy = -dy; 
-			this.setPositionY(this.getPositionY() + dy);
-			dy = 0;
+		oldPosition = this.getPosition();
+		this.setPositionY( this.getPositionY() - this.dy );
+		if( this.estEnCollisionAvecPlateforme(carte) && ! isCollisionOnTop ) {
+			this.setPositionY( oldPosition.getY() );
+			this.dy = 0;	// on remet l'accélération à 0
 		}
-
-			//this.setPosition(this.getPositionX() + dx, this.getPositionY() - dy);
-			
 		
-		// les éléments ramassables peuvent disparaître, on utilise une boucle permettant de supprimer les éléments pendant le parcours
-		/*for( Iterator<ElementRamassable> iterateur = carte.getElementsRamassables().iterator(); iterateur.hasNext(); ) {
-			
-			ElementRamassable ramassable = iterateur.next();
-			
-			if( this.estEnCollisionAvec(ramassable) ) {			
-				this.nbPoints += ramassable.getNbPoints();
-				iterateur.remove();			// on supprime l'élément ramassable de la carte
-			}
-			
-		}			
-
-		// le personnage touche une porte, le jeu est terminé
-		for( Porte porte : carte.getPortes() ) {
-			//if( this.estEnCollisionAvec(porte) ) throw new PartieGagneeException();	
-		}
-
+		
+		
+		
+		
+		// on autorise le personnage à se déplacer une seule fois
+		if( this.direction == Direction.DROITE && this.isMoving ) this.dx = this.vx; 	// déplacement à droite
+		else if( direction == Direction.GAUCHE && this.isMoving ) this.dx = - this.vx; 	// déplacement à gauche
+		else this.dx = 0;
 				
-		for( Ennemi ennemi : carte.getEnnemis() ) {	
-			//if( this.estEnCollisionAvec(ennemi) ) throw new PartiePerdueException();
-
-		}
-				
+		oldPosition = this.getPosition();		// on sauvegarde l'ancienne position
+		
+		this.setPositionX( this.getPositionX() + this.dx );
+		if( this.estEnCollisionAvecPlateforme(carte) && ! this.isCollisionOnTop ) this.setPositionX( oldPosition.getX() );
+		
+		
 					
+		
 		ElementRamassable elementRamassable = carte.getElementRamassableEnCollisionAvecElement(this); 
 		if( elementRamassable != null ) {
 			this.nbPoints += elementRamassable.getNbPoints();
 			carte.supprimerElementRamassable(elementRamassable);
-		}*/
-		
-		/*
-		if( carte.elementEnCollisionAvecUnElementFixe(this) ) {
-			jumping = false; // on dit que le personnage est au sol
-			estEnCollision = true;
 		}
-		*/
 		
 		// le personnage touche une porte, le joueur gagne la partie
 		//if( carte.elementEnCollisionAvecUnePortes(this) ) throw new PartieGagneeException();
@@ -204,7 +147,8 @@ public class Personnage extends ElementDeplacable {
 		for( ElementFixe plateforme : carte.getElementsFixes() ) {	
 			if( this.estEnCollisionAvec(plateforme) ){
 				
-				if(this.getPositionY() - plateforme.getPositionY() <= 0.1){ // collision en haut
+				// collision en haut
+				if( this.getPositionY() - plateforme.getPositionY() <= 0.1 ) {
 					this.setPositionY(plateforme.getPositionY() - this.getHauteur());
 					isCollisionOnTop = true;
 					jumping = false;
