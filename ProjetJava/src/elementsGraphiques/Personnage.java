90c6f896ae1df4527dceda197178f42f9b295005
syffer <maxime.nicolas2@hotmail.fr>
2015-05-30 16:34:48 +0200

    Revert "conflit"
    This reverts commit 24f4b76cba731da7f86df74116bfdc25e5383b30.

--- ProjetJava/src/elementsGraphiques/Personnage.java
+++ ProjetJava/src/elementsGraphiques/Personnage.java
@@ -43,6 +43,25 @@ public class Personnage extends ElementDeplacable {
 
 	
 	/**
+	 * La suite de points (x,y) permettant de contruire les hitbox du personnage.
+	 */
+	private static final float[] POSITIONS_HITBOX_GAUCHE = { 6, 0, 9, 2, 14, 7, 14, 10, 13, 12, 11, 14, 14, 17, 17, 18, 21, 24, 24, 21, 27, 25, 27, 28,       25, 32, 3, 32,      2, 19, 4, 14, 1, 10, 1, 7, 2, 5, 4, 3 }; 
+	private static final float[] POSITIONS_HITBOX_DROITE = { 25, 0, 30, 7, 30, 10, 27, 15, 29, 18, 28, 26,       27, 32, 6, 32,     4, 28, 4, 25, 7, 21, 10, 24, 14, 18, 17, 17, 20, 14, 17, 10, 17, 7, 20, 3 };
+	private static final float[] POSITIONS_HITBOX_DEPLACEMENT_GAUCHE = { 2, 1, 4, 1, 6, 3, 11, 3, 16, 8, 16, 11, 14, 14, 15, 16, 20, 17, 20, 22, 26, 30, 27, 24,       27, 32, 5, 32,       5, 26, 6, 23, 3, 20, 6, 15, 3, 11, 3, 8, 4, 5 };  
+	private static final float[] POSITIONS_HITBOX_DEPLACEMENT_DROITE = { 29, 1, 27, 5, 28, 8, 28, 11, 25, 15, 28, 19, 25, 23, 26, 26,            26, 32, 4, 32,      4, 24, 5, 20, 11, 27, 11, 17, 18, 15, 15, 11, 15, 8, 20, 3, 25, 3, 27, 1 };
+	private static final float[] POSITIONS_HITBOX_SAUT_GAUCHE = { 0, 0, 2, 0, 4, 2, 8, 2, 13, 7, 13, 10, 15, 12, 19, 15, 19, 19, 24, 19, 24, 23, 28, 23,      28, 32, 18, 32,     8, 23, 8, 20, 3, 18, 3, 14, 0, 10 };
+	private static final float[] POSITIONS_HITBOX_SAUT_DROITE = { 31, 0, 29, 0, 27, 2, 23, 2, 18, 7, 18, 10, 16, 12, 12, 15, 12, 19, 7, 19, 7, 23, 3, 23,     3, 32, 13, 32,    23, 23, 23, 20, 28, 18, 28, 14, 31, 10 };
+	
+	
+	private static final int FIXE_GAUCHE = 0;
+	private static final int FIXE_DROITE = 1;
+	private static final int DEPLACEMENT_GAUCHE = 2;
+	private static final int DEPLACEMENT_DROITE = 3;
+	private static final int SAUT_GAUCHE = 4;
+	private static final int SAUT_DROITE = 5;
+	
+	
+	/**
 	 * @param x La position en x du personnage
 	 * @param y La position en y du personnage
 	 */
@@ -82,26 +101,23 @@ public class Personnage extends ElementDeplacable {
 		
 		this.sprite = new SpriteSheet( this.cheminSprite, 32, 32 );
 		
-		this.animations = new Animation[6];
+		this.animations = new Animation[6];		
+		this.animations[ Personnage.FIXE_GAUCHE ] = this.chargerAnimation( 0, 0, 1 );	// fixe gauche
+		this.animations[ Personnage.FIXE_DROITE ] = this.chargerAnimation( 1, 0, 1 );	// fixe droite
+		this.animations[ Personnage.DEPLACEMENT_GAUCHE ] = this.chargerAnimation( 2, 0, 1 );	// deplacement gauche
+		this.animations[ Personnage.DEPLACEMENT_DROITE ] = this.chargerAnimation( 3, 0, 1 ); 	// deplacement droite
+		this.animations[ Personnage.SAUT_GAUCHE ] = this.chargerAnimation( 4, 0, 0 );	// saut gauche
+		this.animations[ Personnage.SAUT_DROITE ] = this.chargerAnimation( 4, 1, 1 );	// saut droite
+				
+		this.hitboxs = new Hitbox[6];
+		this.hitboxs[ Personnage.FIXE_GAUCHE ] = new Hitbox( Personnage.POSITIONS_HITBOX_GAUCHE );
+		this.hitboxs[ Personnage.FIXE_DROITE ] = new Hitbox( Personnage.POSITIONS_HITBOX_DROITE );
+		this.hitboxs[ Personnage.DEPLACEMENT_GAUCHE ] = new Hitbox( Personnage.POSITIONS_HITBOX_DEPLACEMENT_GAUCHE );
+		this.hitboxs[ Personnage.DEPLACEMENT_DROITE ] = new Hitbox( Personnage.POSITIONS_HITBOX_DEPLACEMENT_DROITE );
+		this.hitboxs[ Personnage.SAUT_GAUCHE ] = new Hitbox( Personnage.POSITIONS_HITBOX_SAUT_GAUCHE );
+		this.hitboxs[ Personnage.SAUT_DROITE ] = new Hitbox( Personnage.POSITIONS_HITBOX_SAUT_DROITE );
 		
-		Animation anim;
-		//déclaration des 4 premières animations : immobile et déplacement
-		for(int i=0;i<4;i++){
-			anim = new Animation();
-			anim.addFrame(this.sprite.getSprite(0, i),250);
-			anim.addFrame(this.sprite.getSprite(1, i),250);
-			this.animations[i] = anim;
-		}
-		//5Ã¨me animation : sauter à  gauche
-		anim = new Animation();
-		anim.addFrame(this.sprite.getSprite(0, 4), 1000);
-		this.animations[4] = anim;
-		
-		//Animation : sauter à  droite
-		anim = new Animation();
-		anim.addFrame(this.sprite.getSprite(1, 4), 1000);
-		this.animations[5] = anim;
-		
+		this.setHitbox( this.hitboxs[ this.situationAnimation ] );
 	}
 
 	
