8420b85507525557269421c53f21494969f4365c
syffer <maxime.nicolas2@hotmail.fr>
2015-05-31 20:54:26 +0200

    renommage

--- ProjetJava/POUR LE JAR/start.sh
+++ ProjetJava/POUR LE JAR/start.sh
@@ -1,4 +1,4 @@
 
-java -Djava.library.path="Ressources/libs/natives" -jar TheCaterpillarQuest_jarsplice.jar Max
+java -Djava.library.path="Ressources/libs/natives" -jar TheCaterpillarQuest.jar Max
 
 #java -cp TheCaterpillarQuest.jar app.Application Max
