8bbee9d031bd5f51208ce4e8533d943a3e794956
CyCaron <caron.cyril73@gmail.com>
2015-05-31 16:37:36 +0200

    commentaires javadoc enum Direction

--- ProjetJava/src/elementsGraphiques/Direction.java
+++ ProjetJava/src/elementsGraphiques/Direction.java
@@ -3,7 +3,8 @@ package elementsGraphiques;
 
 /**
  * @author Cyril Caron
- *
+ * 
+ * Enumération représentant les différents états dans lequel le personnage se trouve selon les touches appuyées sur le clavier.
  */
 public enum Direction {
 
